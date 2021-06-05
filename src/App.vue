e2e39fa37525262d24bed9cedb51ebd3fdd735d9
xizeyoupan <44920131+xizeyoupan@users.noreply.github.com>
2021-06-05 22:44:33 +0800

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import trianglify from "trianglify";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "app",
+  created: function () {
+    const pattern = trianglify({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    const canvas = pattern.toCanvas();
+    canvas.id = "lowpoly";
+    document.body.appendChild(canvas);
+  },
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="less" scoped>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button, Form, FormItem, Input } from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
